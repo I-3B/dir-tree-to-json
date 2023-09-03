11f8afd8dc7010f1300f0bde68c1a9227eaeb6ea
Islam Naasani <islamnaasani@gmail.com>
2023-09-04 00:14:01 +0300

    feat: add --only-directory and --extensions options

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jskeq
+# dir-tree-to-json
 
 a node script to convert a directory structure to a json file
 
@@ -9,3 +9,21 @@ npx dir-tree-to-json <directory-path>
 ```
 
 which would generate a json file representing the file structure of the passed directory
+
+## Options
+
+### `--only-directory`
+
+```bash
+npx dir-tree-to-json <directory-path> --only-directory
+```
+
+Will only output directories and omit files
+
+### `--extensions`
+
+```bash
+npx dir-tree-to-json <directory-path> --extensions=mp4,json
+```
+
+Will only output files that ends with the provided extensions, if `--only-directory`` is present, this flag will be ignored
--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,9 @@
 const fs = require("fs");
 const path = require("path");
 
-function readDirectory(directoryPath) {
-  const result = {};
+function readDirectory(directoryPath, options) {
+  const stats = fs.statSync(directoryPath);
+  const result = { size: humanFileSize(stats.size), children: {} };
 
   try {
     const files = fs.readdirSync(directoryPath);
@@ -13,18 +14,25 @@ function readDirectory(directoryPath) {
       try {
         const stats = fs.statSync(filePath);
         if (stats.isDirectory()) {
-          result[file] = readDirectory(filePath);
-        } else {
-          result[file] = humanFileSize(stats.size);
+          result.children[file] = readDirectory(filePath, options);
+        } else if (!options.onlyDirectory) {
+          if (
+            options.extensions.length === 0 ||
+            options.extensions.includes(path.extname(file).slice(1))
+          ) {
+            result.children[file] = humanFileSize(stats.size);
+          }
         }
-      } catch {
+      } catch (e) {
+        console.log(e);
         result[file] = "Couldn't Read!";
       }
     });
-  } catch {
+  } catch (e) {
+    console.log(e);
     result = "Couldn't Read!";
   }
-
+  if (Object.keys(result.children).length === 0) delete result.children;
   return result;
 }
 function humanFileSize(bytes, si = false, dp = 1) {
@@ -54,10 +62,22 @@ function exportToJson(data, outputPath) {
 }
 
 const directoryPath = process.argv[2];
+const options = {
+  extensions: [],
+  onlyDirectory: false,
+};
+process.argv.slice(3).forEach((arg) => {
+  if (arg === "--only-directory") {
+    options.onlyDirectory = true;
+  } else if (arg.startsWith("--extension")) {
+    const extensionArg = arg.slice(arg.indexOf("=") + 1);
+    options.extensions = extensionArg.split(",");
+  }
+});
 if (!directoryPath) {
   console.error("Please provide a directory path.");
   process.exit(1);
 }
 
-const data = readDirectory(directoryPath);
+const data = readDirectory(directoryPath, options);
 exportToJson(data, `${path.join(process.cwd(), path.parse(directoryPath).name)}.json`);
